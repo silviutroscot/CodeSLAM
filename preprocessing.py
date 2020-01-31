6ff17065b0483496e36deb7c2c3ae5e88efaccc1
sitros <silviu.troscot@gstechnologies.io>
2020-01-31 01:39:48 +0000

    added resizing for the intensity images:

--- preprocessing.py
+++ preprocessing.py
@@ -8,7 +8,7 @@ print(TRAINING_SET_PATH)
 
 def create_intensity_images_from_rgb_images_folder(path):
     print("create intensity is called")
-    for filename in glob.iglob(path + '**/*/photo/*', recursive=True):
+    for filename in glob.iglob(path + '**/0/photo/*', recursive=True):
         # don't duplicate intensity images
         if not filename.endswith("_intensity.jpg"):
             img = Image.open(filename).convert('L')
@@ -18,16 +18,21 @@ def create_intensity_images_from_rgb_images_folder(path):
             img.save(intensity_image_name)
 
 def resize_intensity_images(path, new_width, new_height):
-    for filename in glob.iglob(path + '**/*/photo/*_intensity.jpg', recursive=True):
+    for filename in glob.iglob(path + '**/0/photo/*_intensity.jpg', recursive=True):
         print(filename)
         img = Image.open(filename)
-        img.resize((new_width, new_height), Image.ANTIALIAS)
+        resized_image = img.resize((new_width, new_height))
         image_name_without_extension = filename.split('.')[0]
         resized_intensity_image_name = image_name_without_extension + '_resized.jpg'
-        img.save(resized_intensity_image_name)
+        resized_image.save(resized_intensity_image_name, "JPEG", optimize=True)
         # remove the original intensity image
+        #os.remove(filename)
+
+def remove_unresized_intensity_images(path):
+    for filename in glob.iglob(path + '**/0/photo/*_intensity.jpg', recursive=True):
         os.remove(filename)
 
 if __name__ == '__main__':
-    create_intensity_images_from_rgb_images_folder(TRAINING_SET_PATH)
-    #resize_intensity_images(TRAINING_SET_PATH, 256, 192)
+    #create_intensity_images_from_rgb_images_folder(TRAINING_SET_PATH)
+    resize_intensity_images(TRAINING_SET_PATH, 256, 192)
+    #remove_resized_images(TRAINING_SET_PATH)
