842ebd3c0e772c0a9611be9f40fc0b2c93ff1636
sitros <silviu.troscot@gstechnologies.io>
2020-02-07 23:36:19 +0000

    added preprocessing for scaling depth in [0, 1]

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,9 @@
 import glob
 import multiprocessing as mp
+import numpy as np
 import os
 from PIL import Image
+import statistics
 import sys
 
 
@@ -32,6 +34,29 @@ def resize_intensity_images(path, new_width, new_height, subfolder):
         # remove the original intensity image
         os.remove(filename)
 
+def scale_depth(image, average):
+    for i in [0, len(image)-1]:
+        image[i] = average / (average + image[i])
+
+def normalize_depth_values(path, subfolder):
+    for filename in glob.iglob(path + str(subfolder) + '/*/depth/*', recursive=True):
+        print(filename)
+        img = Image.open(filename)
+        size = img.size
+        image_values = img.histogram()
+        average_depth = statistics.mean(image_values)
+        scale_depth(image_values, average_depth)
+        image_array = np.array(image_values, dtype=np.float32)
+        
+        image = Image.new("L", size)
+        image.putdata(image_array)
+        normalized_image_name = filename.split('.')[0] + '_normalized.png'
+        image.save(normalized_image_name, "PNG", optimize=True)
+
+def remove_normalized_depth_images(path):
+     for filename in glob.iglob(path + '**/*/depth/*_normalized.png', recursive=True):
+        os.remove(filename)
+
 def remove_intensity_images(path):
     for filename in glob.iglob(path + '**/*/photo/*_intensity.jpg', recursive=True):
         os.remove(filename)
@@ -41,23 +66,42 @@ def remove_intensity_images(path):
 if __name__ == '__main__':
     pool = mp.Pool(mp.cpu_count())
     # if no args are passed, don't alter images
-    if len(sys.argv) is 0:
+    if len(sys.argv) is 1:
         print("You should specify resizing (r) or converting to intensity (i).")
-    elif len(sys.argv) is 1:
-        if str(sys.argv[0]) is "i":
-            [pool.apply(create_intensity_images_from_rgb_images_folder, args=(TRAINING_SET_PATH, subfolder)) for subfolder in DATASET_SUBFOLDERS]
-        elif str(sys.argv[0]) is "r":
-            [pool.apply(resize_intensity_images, args=(TRAINING_SET_PATH, IMAGE_NEW_WIDTH, IMAGE_NEW_HEIGHT, subfolder)) for subfolder in DATASET_SUBFOLDERS]
-        else:
-            print("Invalid argument: use 'i' for convert images to intensity images, 'r' to resize the intensity images or 'i' and 'r' for both")
     elif len(sys.argv) is 2:
-        if (str(sys.argv[0]) is "i" and str(sys.argv[1]) is "r") or (str(sys.argv[0]) is "i" and str(sys.argv[1]) is "r"):
+        if str(sys.argv[1]) is "i":
+            [pool.apply(create_intensity_images_from_rgb_images_folder, args=(TRAINING_SET_PATH, subfolder)) for subfolder in DATASET_SUBFOLDERS]
+        elif str(sys.argv[1]) is "r":
+            [pool.apply(resize_intensity_images, args=(TRAINING_SET_PATH, IMAGE_NEW_WIDTH, IMAGE_NEW_HEIGHT, subfolder)) for subfolder in DATASET_SUBFOLDERS]
+        elif str(sys.argv[1]) is "n":
+            [pool.apply(normalize_depth_values, args=(TRAINING_SET_PATH, subfolder)) for subfolder in DATASET_SUBFOLDERS]
+        else:
+            print("Invalid argument: use 'i' for convert images to intensity images, 'r' to resize the intensity images, 'n' for depth normalization or any combination of them")
+    elif len(sys.argv) is 3:
+        if (str(sys.argv[1]) is "i" and str(sys.argv[2]) is "r") or (str(sys.argv[2]) is "i" and str(sys.argv[1]) is "r"):
             [pool.apply(create_intensity_images_from_rgb_images_folder, args=(TRAINING_SET_PATH, subfolder)) for subfolder in DATASET_SUBFOLDERS]
             [pool.apply(resize_intensity_images, args=(TRAINING_SET_PATH, IMAGE_NEW_WIDTH, IMAGE_NEW_HEIGHT, subfolder)) for subfolder in DATASET_SUBFOLDERS]
+        elif (str(sys.argv[1]) is "i" and str(sys.argv[2]) is "n") or (str(sys.argv[1]) is "n" and str(sys.argv[2]) is "i"):
+            [pool.apply(create_intensity_images_from_rgb_images_folder, args=(TRAINING_SET_PATH, subfolder)) for subfolder in DATASET_SUBFOLDERS]
+            [pool.apply(normalize_depth_values, args=(TRAINING_SET_PATH, subfolder)) for subfolder in DATASET_SUBFOLDERS]
+        elif (str(sys.argv[1]) is "r" and str(sys.argv[2]) is "n") or (str(sys.argv[1]) is "n" and str(sys.argv[2]) is "r"):
+            [pool.apply(resize_intensity_images, args=(TRAINING_SET_PATH, IMAGE_NEW_WIDTH, IMAGE_NEW_HEIGHT, subfolder)) for subfolder in DATASET_SUBFOLDERS]
+            [pool.apply(normalize_depth_values, args=(TRAINING_SET_PATH, subfolder)) for subfolder in DATASET_SUBFOLDERS]
         else:
-            print("Invalid arguments: use 'i' for convert images to intensity images, 'r' to resize the intensity images or 'i' and 'r' for both")
+            print("Invalid arguments: use 'i' for convert images to intensity images, 'r' to resize the intensity images, 'n' for depth normalization or any combination of them")
+    elif len(sys.argv) is 4:
+        if ((str(sys.argv[1]) is "i" and str(sys.argv[2]) is "r" and str(sys.argv[3]) is "n") or
+            (str(sys.argv[1]) is "i" and str(sys.argv[2]) is "n" and str(sys.argv[3]) is "r") or 
+            (str(sys.argv[1]) is "r" and str(sys.argv[2]) is "i" and str(sys.argv[3]) is "n") or 
+            (str(sys.argv[1]) is "r" and str(sys.argv[2]) is "n" and str(sys.argv[3]) is "i") or 
+            (str(sys.argv[1]) is "n" and str(sys.argv[2]) is "i" and str(sys.argv[3]) is "r") or 
+            (str(sys.argv[1]) is "n" and str(sys.argv[2]) is "r" and str(sys.argv[3]) is "i")):
+
+            [pool.apply(create_intensity_images_from_rgb_images_folder, args=(TRAINING_SET_PATH, subfolder)) for subfolder in DATASET_SUBFOLDERS]
+            [pool.apply(resize_intensity_images, args=(TRAINING_SET_PATH, IMAGE_NEW_WIDTH, IMAGE_NEW_HEIGHT, subfolder)) for subfolder in DATASET_SUBFOLDERS]
+            [pool.apply(normalize_depth_values, args=(TRAINING_SET_PATH, subfolder)) for subfolder in DATASET_SUBFOLDERS]
     else:
-        print("Too many arguments: use 'i' for convert images to intensity images, 'r' to resize the intensity images or 'i' and 'r' for both")
+        print("Too many arguments: use 'i' for convert images to intensity images, 'r' to resize the intensity images, 'n' for depth normalization or any combination of them")
     
     pool.close()
     
